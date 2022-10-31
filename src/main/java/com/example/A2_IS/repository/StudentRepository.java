24f8e60096cba608efdce5405a8c78c7701c0c02
Klemen Poličar <klemen.janez.policar@gmail.com>
2022-10-31 17:02:29 +0000

    add req7

--- src/main/java/com/example/A2_IS/repository/StudentRepository.java
+++ src/main/java/com/example/A2_IS/repository/StudentRepository.java
@@ -1,11 +1,7 @@
 package com.example.A2_IS.repository;
 
 import com.example.A2_IS.models.Student;
-import io.r2dbc.spi.Parameter;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
 
 
 public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
