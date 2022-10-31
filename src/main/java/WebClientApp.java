f360d0f498432a2ba0b62e699488e358056c7cdb
Pedro Rodrigues <pedro.rodrigues@onesource.pt>
2022-10-31 10:33:45 +0000

    webclient next features

--- src/main/java/WebClientApp.java
+++ src/main/java/WebClientApp.java
@@ -1,20 +1,77 @@
 import com.example.A2_IS.models.Student;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WebClientApp {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         WebClient client = WebClient.create("http://localhost:8080");
 
-        System.out.println(">>>>> Student List <<<<<");
+//            File f = new File("filename.txt");
+//        try {
+//            if (f.createNewFile()) {
+//                System.out.println("File created: " + f.getName());
+//            } else {
+//                System.out.println("File already exists.");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Students.txt",true));
+
         var headersSpec = client.get()
-            .uri("/a2/student")
-            .retrieve()
-            .bodyToFlux(Student.class)
-            .subscribe(student -> {
-                System.out.println(student.getName());
-            });
+                .uri("/a2/student")
+                .retrieve()
+                .bodyToFlux(Student.class)
+                .subscribe(student -> {
+                    try {
+                        writer.append("\nName: " + student.getName() + "\t Date of Birth: " + student.getDob());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(student.getName());
+                });
 
         Thread.sleep(1500);
+
+
+        var totalOfStudents = client.get()
+                .uri("/a2/student")
+                .retrieve()
+                .bodyToFlux(Student.class)
+                .count()
+                .block();
+
+        writer.write("\nTotal of Students: " + totalOfStudents);
+
+        Thread.sleep(1500);
+
+        var totalOfActiveStudents = client.get()
+                .uri("/a2/student")
+                .retrieve()
+                .bodyToFlux(Student.class)
+                .map(s-> s.getCredits() < 180)
+                .count()
+                .block();
+
+        writer.write("\nTotal number of students that are active: " + totalOfActiveStudents);
+
+        Thread.sleep(1500);
+
+        var completedCourses = client.get()
+                .uri("/a2/student")
+                .retrieve()
+                .bodyToFlux(Student.class)
+                .subscribe(student -> {
+                    int completedCouses = student.getCredits() % 6;
+                });
+
+        writer.close();
+
     }
 }
