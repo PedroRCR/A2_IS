504e43cf2e16e625b707e527d4ba3cd66181af17
Klemen Poličar <klemen.janez.policar@gmail.com>
2022-10-31 16:23:26 +0000

    refactor web client, log each req in its own file

--- src/main/java/WebClientApp.java
+++ src/main/java/WebClientApp.java
@@ -7,82 +7,114 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class WebClientApp {
-    public static void main(String[] args) throws InterruptedException, IOException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException, IOException, FileNotFoundException {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-//            File f = new File("filename.txt");
-//        try {
-//            if (f.createNewFile()) {
-//                System.out.println("File created: " + f.getName());
-//            } else {
-//                System.out.println("File already exists.");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
         PrintStream o = new PrintStream("Students.txt");
         System.setOut(o);
 
-        Future<?> future1 = executorService.submit(WebClientApp::req1);
-        Future<?> future2 = executorService.submit(WebClientApp::req2);
-        Future<?> future3 = executorService.submit(WebClientApp::req3);
-        Future<?> future4 = executorService.submit(WebClientApp::req4);
+        executorService.submit(WebClientApp::req1);
+        executorService.submit(WebClientApp::req2);
+        executorService.submit(WebClientApp::req3);
+        executorService.submit(WebClientApp::req4);
+        executorService.submit(WebClientApp::req6);
 
         Thread.sleep(1500);
         executorService.shutdown();
     }
 
     public static void req1() {
-        WebClient client = WebClient.create("http://localhost:8080");
+        try {
+            WebClient client = WebClient.create("http://localhost:8080");
+            PrintStream o = new PrintStream("Students.req1.txt");
 
-        var headersSpec = client.get()
-                .uri("/a2/student")
-                .retrieve()
-                .bodyToFlux(Student.class)
-                .subscribe(student -> {
-                    System.out.println("\nName: " + student.getName() + "\t Date of Birth: " + student.getDob());
-                    System.out.println(student.getName());
-                });
+            var headersSpec = client.get()
+                    .uri("/a2/student")
+                    .retrieve()
+                    .bodyToFlux(Student.class)
+                    .subscribe(student -> {
+                        o.println("\nName: " + student.getName() + "\t Date of Birth: " + student.getDob());
+                        o.println(student.getName());
+                    });
+        } catch (Exception e) {
+            System.out.println("REQ6 FUNCTION ERROR");
+            e.printStackTrace();
+        }
     }
 
     public static void req2() {
-        WebClient client = WebClient.create("http://localhost:8080");
+        try {
+            WebClient client = WebClient.create("http://localhost:8080");
+            PrintStream o = new PrintStream("Students.req2.txt");
 
-        var totalOfStudents = client.get()
-                .uri("/a2/student")
-                .retrieve()
-                .bodyToFlux(Student.class)
-                .count()
-                .block();
+            var totalOfStudents = client.get()
+                    .uri("/a2/student")
+                    .retrieve()
+                    .bodyToFlux(Student.class)
+                    .count()
+                    .block();
 
-        System.out.println("\nTotal of Students: " + totalOfStudents);
+            o.println("\nTotal of Students: " + totalOfStudents);
+        } catch (Exception e) {
+            System.out.println("REQ6 FUNCTION ERROR");
+            e.printStackTrace();
+        }
     }
 
     public static void req3() {
-        WebClient client = WebClient.create("http://localhost:8080");
+        try {
+            WebClient client = WebClient.create("http://localhost:8080");
+            PrintStream o = new PrintStream("Students.req3.txt");
 
-        var totalOfActiveStudents = client.get()
-                .uri("/a2/student")
-                .retrieve()
-                .bodyToFlux(Student.class)
-                .map(s-> s.getCredits() < 180)
-                .count()
-                .block();
+            var totalOfActiveStudents = client.get()
+                    .uri("/a2/student")
+                    .retrieve()
+                    .bodyToFlux(Student.class)
+                    .map(s -> s.getCredits() < 180)
+                    .count()
+                    .block();
 
-        System.out.println("\nTotal number of students that are active: " + totalOfActiveStudents);
+            o.println("\nTotal number of students that are active: " + totalOfActiveStudents);
+        } catch (Exception e) {
+            System.out.println("REQ6 FUNCTION ERROR");
+            e.printStackTrace();
+        }
     }
 
     public static void req4() {
-        WebClient client = WebClient.create("http://localhost:8080");
+        try {
+            WebClient client = WebClient.create("http://localhost:8080");
+            PrintStream o = new PrintStream("Students.req4.txt");
 
-        var completedCourses = client.get()
-                .uri("/a2/student")
-                .retrieve()
-                .bodyToFlux(Student.class)
-                .subscribe(student -> {
-                    int completedCouses = student.getCredits() / 6;
+            var completedCourses = client.get()
+                    .uri("/a2/student")
+                    .retrieve()
+                    .bodyToFlux(Student.class)
+                    .subscribe(student -> {
+                        int completedCouses = student.getCredits() / 6;
 
-                    System.out.println("\nName of the student: " + student.getName() + " number of completed couses: " + completedCouses);
-                });
+                        o.println("\nName of the student: " + student.getName() + " number of completed couses: " + completedCouses);
+                    });
+        } catch (Exception e) {
+            System.out.println("REQ6 FUNCTION ERROR");
+            e.printStackTrace();
+        }
+    }
+
+    public static void req6() {
+        try {
+            WebClient client = WebClient.create("http://localhost:8080");
+            PrintStream o = new PrintStream("Students.req6.txt");
+
+            var studentGrades = client.get()
+                    .uri("/a2/student")
+                    .retrieve()
+                    .bodyToFlux(Student.class)
+                    .map(Student::getAvarage);
+
+            var avgGrade = studentGrades.reduce(Float::sum).block() / studentGrades.count().block();
+        } catch (Exception e) {
+            System.out.println("REQ6 FUNCTION ERROR");
+            e.printStackTrace();
+        }
     }
 }
